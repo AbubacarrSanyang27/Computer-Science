c5b214e18a846226d052609b20a3999082a8f482
AbubacarrSanyang27 <836953@mystma.org>
2024-09-24 09:46:48 -0500

    sep 24

---
+++ Assignments/Animal Survay.py
@@ -0,0 +1,3 @@
+user_input = input("Whats your favorite Animal: ")
+user_input2 = input("Whats sound do " + user_input + " make: ")
+user_input3 = input("Where do " + user_input + " live")
---
+++ Assignments/Top 5 Movies.py
@@ -0,0 +1,6 @@
+print("These are my top 5 movies")
+print("the lego movie")
+print("the lego movie 2")
+print("the fnaf movie")
+print("the sonic move")
+print(" the mario movie")
