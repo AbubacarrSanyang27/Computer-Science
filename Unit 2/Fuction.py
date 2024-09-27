8c9975040607bda928e54ea4a718234f8969f09f
AbubacarrSanyang27 <836953@mystma.org>
2024-09-27 09:45:55 -0500

    tax

---
+++ Unit 2/Fuction.py
@@ -0,0 +1,10 @@
+def top_5_Movies(mov1, mov2, mov3, mov4, mov5):
+    print(" Your #1 pick for movies is " + mov1)  #indented lines are in the function
+    print(" Your #2 pick for movies is " + mov2)
+    print(" Your #3 pick for movies is " + mov3)
+    print(" Your #4 pick for movies is " + mov4)
+    print(" Your #5 pick for movies is " + mov5)
+
+top_5_Movies("Lego Movie", "Mario Movie", "Sonic Movie" , "Megamind", "Shrek")
+
+
---
+++ Unit 2/tax.py
@@ -0,0 +1,5 @@
+def calulate_tax(item, price, rate):
+   print(item)
+   print(int( price + rate))
+
+   calulate_tax("water", "1", "6.875")
