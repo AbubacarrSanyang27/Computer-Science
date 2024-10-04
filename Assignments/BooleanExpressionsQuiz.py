52b28a52b6801830ad14754533649ed87e1013ce
AbubacarrSanyang27 <836953@mystma.org>
2024-10-04 09:19:01 -0500

    assignment

---
+++ Assignments/BooleanExpressionsQuiz.py
@@ -0,0 +1,19 @@
+answer = input("Give a whole number that is greater than 19.\n")
+answer = int(answer)
+print(answer > 19)
+
+answer2 = input("Give a whole number that is less than 48.\n")
+answer2 = int(answer2)
+print(answer2 < 48)
+
+answer3 = input("Give a whole number that is less than 100.\n")
+answer3 = int(answer3)
+print(answer3 < 100)
+
+answer4 = input("Give a whole number that is greater than 7.\n")
+answer4 = int(answer4)
+print(answer4 > 7)
+
+answer5 = input("Give a whole number that is equal to 50.\n")
+answer5 = int(answer5)
+print(answer5 == 50)
