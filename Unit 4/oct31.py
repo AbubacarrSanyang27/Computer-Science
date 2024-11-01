c9cbd7f86ceb83449f94d10fb5ddb9a3273e1b2c
AbubacarrSanyang27 <836953@mystma.org>
2024-11-01 08:43:13 -0500

    text

---
+++ Unit 4/oct31.py
@@ -0,0 +1,25 @@
+#Python has four types of collections
+#Tuple
+#Set
+#List
+#Dictionary
+#INDEX                     0       1          2       3       4
+Best_Minecraft_Weapon = ["Mace", "Sword", "Trident", "Bow", "Axe"]
+
+print(Best_Minecraft_Weapon)
+
+print(Best_Minecraft_Weapon[3])
+
+(Best_Minecraft_Weapon[3]) = "TNT"
+
+print(Best_Minecraft_Weapon)
+
+
+Best_Minecraft_Weapon.remove("Axe")
+print(Best_Minecraft_Weapon)
+
+Best_Minecraft_Weapon.append("Bow")
+print(Best_Minecraft_Weapon)
+
+
+Why_I_Hate_SNOW = 
